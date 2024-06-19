2c45732f90640611059b76435befc99c2e3027fc
Biswajeet yadav <biswajeetyadavofficial@gmail.com>
2024-06-19 21:55:41 +0530

    adding new file factorial_recursion to the repo

---
+++ recursion/factorial_recursion.java
@@ -0,0 +1,13 @@
+package recursion;
+
+public class factorial_recursion {
+    public static void main(String args[]){
+        System.out.println(factorial(5));
+    }
+    public static int factorial(int n){
+        if(n == 0){
+            return 1;
+        }
+        return n * factorial(n-1);
+    }
+}
