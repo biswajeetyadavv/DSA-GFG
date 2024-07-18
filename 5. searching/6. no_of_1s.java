eefbba97f979e64b61d33676a29407a9e551379a
Biswajeet yadav <biswajeetyadavofficial@gmail.com>
2024-07-18 22:38:56 +0530

    adding new file to searching

---
+++ 5. searching/6. no_of_1s.java
@@ -0,0 +1,54 @@
+package seraching;
+
+public class no_of_1s {
+    public static void main(String args[]){
+        int[] arr = {0,0,0,1,1,1,1,1};
+        count_1s(arr,0,arr.length,1);
+    }
+    public static int count_1s(int[] arr,int start,int end,int target){
+        int first = first_occ(arr,start,end,target);
+        if(first == -1){
+            return 0;
+        }
+        else{
+            return last_occ(arr,start,end,target)-first +1;
+        }
+    }
+
+
+    public static int first_occ(int[] arr,int start,int end,int target){
+        int mid = (start + end)/2;
+        if(arr[mid] > target){
+            return end = mid -1;
+        }
+        else if(arr[mid]< target){
+            return start = mid+1;
+        }
+        else{
+            if(mid==0||arr[mid-1] != arr[mid]){
+                return mid;
+            }
+            else{
+                return first_occ(arr,start,mid -1,target);
+            }
+        }
+    }
+
+    public static int last_occ(int[] arr,int start,int end,int target){
+        int mid = (start+end)/2;
+        if(arr[mid] > target){
+            return end = mid -1;
+        }
+        else if(arr[mid] < target){
+            return start = mid +1;
+        }
+        else{
+            if(mid == 0|| arr[mid+1]!=arr[mid]){
+                return mid;
+            }
+            else{
+                return last_occ(arr,mid+1,end,target);
+            }
+        }
+    }
+}
